de54ca68356c59291151db0a61d3d9ec651a732b
krabat1 <istvan.orosz78@gmail.com>
2026-03-03 18:11:12 +0100

    feat: keystatic editor functional with react and markdoc integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import keystatic from '@keystatic/astro';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), markdoc(), keystatic()],
+  //output: 'hybrid',
+});
